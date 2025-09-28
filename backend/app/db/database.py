117e43fd1f8fe9c7b66c0edf0d86170c12667fc2
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2025-09-28 14:21:21 +0530

    adata base test

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,14 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///data.db"  # For SQLite. and plzzz check if other dbase is needed
+# Use ./ so it’s relative to project root
+DATABASE_URL = "sqlite:///./data.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},  # only for SQLite
+    echo=True,   # show SQL queries
+    future=True
+)
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
